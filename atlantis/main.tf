e9482cc6a541331c290e9ef229d85cffb95f3995
subhasis020299 <subhasisdas125@gmail.com>
2023-04-16 02:35:46 +0530

    added IAM instance profile

--- atlantis/main.tf
+++ atlantis/main.tf
@@ -46,6 +46,55 @@ resource "aws_security_group_rule" "all_egress" {
   security_group_id = aws_security_group.atlantis.id
 }
 
+# create an ec2 instance role for the atlantis server
+data "aws_iam_policy_document" "atlantis" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "atlantis" {
+  name   = "atlantis-ec2-instance-policy"
+  policy = data.aws_iam_policy_document.atlantis.json
+
+  tags = {
+    "Terraform" = "true"
+  }
+}
+
+resource "aws_iam_role" "atlantis" {
+  name               = "atlantis-ec2-instance-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+  }
+  EOF
+
+  tags = {
+    "Terraform" = "true"
+  }
+}
+
+resource "aws_iam_instance_profile" "atlantis" {
+  name = "atlantis-ec2-instance-profile"
+  role = aws_iam_role.atlantis.name
+  tags = {
+    "Terraform" = "true"
+  }
+}
+
 # create ec2 instance 
 resource "aws_instance" "atlantis" {
   ami                         = data.aws_ami.ubuntu.id
@@ -53,6 +102,7 @@ resource "aws_instance" "atlantis" {
   key_name                    = local.ssh_key_pair_name
   vpc_security_group_ids      = [aws_security_group.atlantis.id]
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.atlantis.name
   user_data_base64 = base64encode("${templatefile("${path.module}/atlantis-setup.sh", {
     gh_username       = var.gh_username,
     gh_pat            = var.gh_pat,
